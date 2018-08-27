b4dcac946f25d0f661560fdf8505bce5c6b16324
Stephane Maarek <simplesteph@users.noreply.github.com>
2018-08-27 15:20:30 +0200

    high throughput producer

--- src/main/java/com/github/simplesteph/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/github/simplesteph/kafka/tutorial2/TwitterProducer.java
@@ -31,7 +31,7 @@ public class TwitterProducer {
     String token = "2932095118-XtnODAvArwq61KA8ZnirEgBBchd66mWwQIwpblO";
     String secret = "5AM3hVYQH9wr17pGPJjavQS2CFJribm0l2YGWO3zJ2uDS";
 
-    List<String> terms = Lists.newArrayList("kafka");
+    List<String> terms = Lists.newArrayList("bitcoin", "usa", "politics", "sport", "soccer");
 
 
     public TwitterProducer(){}
@@ -126,6 +126,11 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); // kafka 2.0 >= 1.1 so we can keep this as 5. Use 1 otherwise.
+
+        // high throughput producer (at the expense of a bit of latency and CPU usage)
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024)); // 32 KB batch size
 
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
