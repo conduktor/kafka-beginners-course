a9ecceae17ea3361db4659c62d887842af618062
sraakesh95github <raakesh95jobs@gmail.com>
2024-03-25 14:56:44 -0400

    Complete event handler testing

--- kafka-producer-wikimedia-sr/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia-sr/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -7,31 +7,36 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class WikimediaChangesProducer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        String bootstrapServers = "127.0.0.1:9092";
-
-        //create Producer properties
+        // create Producer properties
         Properties properties = new Properties();
 
-        // connect to Localhost
-        // properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-
-        // connect to Conduktor Playground
-        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
-        properties.setProperty("security.protocol", "SASL_SSL");
-        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
-        properties.setProperty("sasl.mechanism", "PLAIN");
-
-        // set producer properties
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        // Upstash cluster properties
+        properties.put("bootstrap.servers", "https://driving-anemone-14951-us1-kafka.upstash.io:9092");
+        properties.put("sasl.mechanism", "SCRAM-SHA-256");
+        properties.put("security.protocol", "SASL_SSL");
+        properties.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ZHJpdmluZy1hbmVtb25lLTE0OTUxJOOUD-QWV9p9W116XVhMgWQkRVx_FvKlBac\" password=\"YWUxMDAyMTAtYWNhOS00OTg5LWEwMmQtOWE4NzdlYmFmZTIx\";");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
         String topic = "wikimedia.recentchange";
+
+        //docker properties
+//        properties.setProperty("bootstrap.servers", "kafka:19092");
+//        properties.setProperty("security.protocol", "SASL_SSL");
+//        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+//        properties.setProperty("sasl.mechanism", "PLAIN");
+//
+        // set producer properties
+        properties.put("key.serializer", StringSerializer.class.getName());
+        properties.put("value.serializer", StringSerializer.class.getName());
+
 
         EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
@@ -42,5 +47,6 @@ public class WikimediaChangesProducer {
         eventSource.start();
 
         //handle the events in the Handler
+        TimeUnit.MINUTES.sleep(10);
     }
 }
