d8d42cd21c3fb0b38ca14cc4bd7346cce9fe5ec8
sraakesh95github <raakesh95jobs@gmail.com>
2024-03-25 14:09:22 -0400

    event handler

---
+++ kafka-producer-wikimedia-sr/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,46 @@
+package io.conduktor.demos.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.EventHandler;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        //create Producer properties
+        Properties properties = new Properties();
+
+        // connect to Localhost
+        // properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // connect to Conduktor Playground
+        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        //start the producer in another thread
+        eventSource.start();
+
+        //handle the events in the Handler
+    }
+}
